f3c04ab697ad02771245dfb78a53c615aab1ee3a
pedromcorreia <pedmcor@gmail.com>
2020-05-03 17:18:37 -0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- mix.exs
+++ mix.exs
@@ -40,7 +40,6 @@ defmodule Pullrequests.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:httpoison, "~> 1.6"}
